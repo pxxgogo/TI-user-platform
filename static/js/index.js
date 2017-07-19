77892cf054dc401b1e8183d664f052043389b8d9
潘星宇 <421915293@163.com>
2017-07-19 14:13:53 +0800

    add time limit

--- static/js/index.js
+++ static/js/index.js
@@ -5,10 +5,24 @@ var gesture_img_list = ["1-wave.png", "2-swipe.png", "3-rotate.png", "4-spread-f
     "6-point.jpg", "7-tap.gif", "8-circle.png", "9-write.jpg", "10-o.jpg", "11-10.png", "12-y.jpg", "13-r.jpg",
     "14-f.jpg", "15-q.jpg"];
 
-var gesture_description_list = ["五指张开，“擦玻璃”", "五指张开，手掌左右扇动", "五指张开，手掌前后反转", "手张开->握拳", "手握拳->张开",
-    "用食指指点：从前方指向右前方，再回到前方", "用食指在空中点击", "用食指画圈", "写字", "字母 O 的手势 (O)", "竖起拇指，左右摇动 (10)", "数字 6 的手势 (Y)",
-    "食指、中指交叉 (R)", "OK 的手势 (F)", "手腕略向下弯曲，拇指和食指指向下 (Q)"];
+var gesture_description_list = [
+    "五指张开，“擦玻璃”",
+    "五指张开，手掌左右扇动",
+    "五指张开，手掌前后反转",
+    "手张开->握拳",
+    "手握拳->张开",
+    "用食指指点：从前方指向右前方，再回到前方",
+    "用食指在空中点击",
+    "用食指画圈",
+    "写字",
+    "字母 O 的手势 (O)",
+    "竖起拇指，左右摇动 (10)",
+    "数字 6 的手势 (Y)",
+    "食指、中指交叉 (R)",
+    "OK 的手势 (F)",
+    "手腕略向下弯曲，拇指和食指指向下 (Q)"];
 
+var gesture_times_list = [];
 var current_gesture_index = -1;
 var experiment_start_flag = false;
 var gesture_record_flag = false;
@@ -17,6 +31,8 @@ var experiment_log_list = [];
 var current_log = null;
 var user_name = "";
 var timer = null;
+var max_times_per_gesture = 15;
+var gesture_times = 0;
 console.log("max gesture index:", gesture_max_index);
 
 $(document).ready(function () {
@@ -42,6 +58,7 @@ function start_experiment() {
             if (experiment_start_flag) {
                 return;
             }
+            init_gesture_times_list();
             user_name = $("#user-name-input").val();
             $("#user-name-input").val("");
             console.log("user name:", user_name);
@@ -58,33 +75,65 @@ function start_experiment() {
 
 }
 
-function stop_experiment() {
-    swal({
-            title: "您确定要终止实验吗？",
-            text: "终止实验后，之前的实验数据将会清空",
+function stop_experiment(flag) {
+    if (flag === 0) {
+        swal({
+                title: "您确定要终止实验吗？",
+                text: "终止实验后，之前的实验数据将会清空",
+                type: "info",
+                showCancelButton: true,
+                confirmButtonColor: "#4f9add",
+                confirmButtonText: "确定",
+                cancelButtonText: "取消",
+                closeOnConfirm: true
+            },
+            function () {
+                if (!experiment_start_flag) {
+                    return;
+                }
+                experiment_start_flag = false;
+                gesture_record_flag = false;
+                $("#start-recording-btn").hide();
+                $("#start-experiment-btn").show();
+                $("#gesture-img").hide();
+                $("#start-img").show();
+                $("#gesture-description").hide();
+                $("#start-description").show();
+                download_logs();
+                experiment_log_list = [];
+
+            });
+    } else {
+        swal({
+            title: "实验完成",
+            text: "感谢您的配合",
             type: "info",
-            showCancelButton: true,
             confirmButtonColor: "#4f9add",
             confirmButtonText: "确定",
-            cancelButtonText: "取消",
             closeOnConfirm: true
-        },
-        function () {
-            if (!experiment_start_flag) {
-                return;
-            }
-            experiment_start_flag = false;
-            gesture_record_flag = false;
-            $("#start-recording-btn").hide();
-            $("#start-experiment-btn").show();
-            $("#gesture-img").hide();
-            $("#start-img").show();
-            $("#gesture-description").hide();
-            $("#start-description").show();
-            download_logs();
-            experiment_log_list = [];
-
         });
+        if (!experiment_start_flag) {
+            return;
+        }
+        experiment_start_flag = false;
+        gesture_record_flag = false;
+        $("#start-recording-btn").hide();
+        $("#start-experiment-btn").show();
+        $("#gesture-img").hide();
+        $("#start-img").show();
+        $("#gesture-description").hide();
+        $("#start-description").show();
+        download_logs();
+        experiment_log_list = [];
+    }
+
+
+}
+
+function init_gesture_times_list() {
+    for (var i = 0; i < gesture_description_list.length; i++)
+        gesture_times_list.push(0);
+    gesture_times = 0;
 
 }
 
@@ -106,8 +155,17 @@ function download_logs() {
 }
 
 
-function get_random_int() {
-    return Math.floor(Math.random() * (gesture_max_index + 1));
+function get_next_gesture() {
+    if (gesture_times >= max_times_per_gesture * (gesture_max_index + 1)) {
+        return -1;
+    }
+    gesture_times += 1;
+    var gesture_index = Math.floor(Math.random() * (gesture_max_index + 1));
+    while (gesture_times_list[gesture_index] >= max_times_per_gesture) {
+        gesture_index = Math.floor(Math.random() * (gesture_max_index + 1));
+    }
+    gesture_times_list[gesture_index] += 1;
+    return gesture_index;
 }
 
 function start_recording() {
@@ -139,19 +197,23 @@ function get_timestamp() {
 }
 
 function update_gesture() {
-    current_gesture_index = get_random_int();
-    console.log("gesture index", current_gesture_index);
-    $("#gesture-img").attr("src", "img/" + gesture_img_list[current_gesture_index]);
-    $("#gesture-description").html(gesture_description_list[current_gesture_index]);
     if (current_log !== null)
         experiment_log_list.push(current_log);
     // console.log(experiment_log_list);
     current_log = {};
+    current_gesture_index = get_next_gesture();
+    if (current_gesture_index === -1) {
+        stop_experiment(1);
+        return;
+    }
+    console.log("gesture index", current_gesture_index);
+    $("#gesture-img").attr("src", "img/" + gesture_img_list[current_gesture_index]);
+    $("#gesture-description").html(gesture_description_list[current_gesture_index]);
     current_log["gesture_index"] = current_gesture_index;
 }
 
 $(document).keypress(function (e) {
-    if(!experiment_start_flag)
+    if (!experiment_start_flag)
         return;
     console.log(e.which);
     var key = e.which;
@@ -162,6 +224,6 @@ $(document).keypress(function (e) {
             start_recording();
         }
     } else if (key === 113) {
-        stop_experiment();
+        stop_experiment(0);
     }
 });
